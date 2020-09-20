03b13f4d752f62d433d6cedc12f537dfdf8f2319
ullyolima <ullyolima@gmail.com>
2020-09-20 16:37:17 -0300

    Add fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,1 @@
-AOS.init() 
-AOS.refreshHard()
-function initMap(){let e;e=new google.maps.Map(document.getElementById("map"),{center:{lat:-23.599225,lng:-46.6366589},zoom:18})}
-
-const modal = document.querySelector("#myModal")
-const form=document.querySelector(".needs-validation");
-form.addEventListener("submit",function(e){!1===form.checkValidity()&&(e.preventDefault(),e.stopPropagation()),form.classList.add("was-validated")})
-
-
-const masks={phone:e=>e.replace(/\D/g,"").replace(/(\d{2})(\d)/,"($1) $2").replace(/(\d{4})(\d)/,"$1-$2").replace(/(\d{4})-(\d)(\d{4})/,"$1$2-$3")};document.querySelectorAll("input").forEach(e=>{const t=e.dataset.js;e.addEventListener("input",e=>{e.target.value=masks[1](e.target.value)},!1)})
-
+const form=document.querySelector(".needs-validation");function initMap(){let e;e=new google.maps.Map(document.getElementById("map"),{center:{lat:-23.599225,lng:-46.6366589},zoom:18})}form.addEventListener("submit",function(e){!1===form.checkValidity()&&(e.preventDefault(),e.stopPropagation()),form.classList.add("was-validated")}),AOS.init();const masks={phone:e=>e.replace(/\D/g,"").replace(/(\d{2})(\d)/,"($1) $2").replace(/(\d{4})(\d)/,"$1-$2").replace(/(\d{4})-(\d)(\d{4})/,"$1$2-$3")};document.querySelectorAll("input").forEach(e=>{const t=e.dataset.js;e.addEventListener("input",e=>{e.target.value=masks[t](e.target.value)},!1)});
